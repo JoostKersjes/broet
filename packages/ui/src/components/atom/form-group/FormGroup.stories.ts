219df2b8307c05ba74c6342cc5d6ba7ba12ecab2
Joost Kersjes <joostkersjes@live.nl>
2023-09-22 19:05:13 +0200

    feat(ui): integrate Tailwind and Storybook

---
+++ packages/ui/src/components/atom/form-group/FormGroup.stories.ts
@@ -0,0 +1,17 @@
+import type { Meta, StoryObj } from "@storybook/vue3";
+import FormGroup from "./FormGroup.vue";
+
+const meta: Meta<typeof FormGroup> = {
+  title: "Atom/Form/Group",
+  component: FormGroup,
+};
+
+export default meta;
+type Story = StoryObj<typeof FormGroup>;
+
+export const Default = {
+  args: {
+    legend: "Group of form elements",
+    default: "some contents",
+  },
+} satisfies Story;
---
+++ packages/ui/src/components/atom/form-group/FormGroup.vue
@@ -0,0 +1,12 @@
+<script lang="ts" setup>
+defineProps<{
+  legend: string;
+}>();
+</script>
+
+<template>
+  <fieldset class="border-2 rounded-lg p-4">
+    <legend class="px-2">{{ legend }}</legend>
+    <slot></slot>
+  </fieldset>
+</template>
