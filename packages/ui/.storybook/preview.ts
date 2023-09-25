b21c7547aca96a029dff8fed27978cd903d5e94d
Joost Kersjes <joostkersjes@live.nl>
2023-09-25 18:25:55 +0200

    chore: clean up some config files

--- packages/ui/.storybook/preview.ts
+++ packages/ui/.storybook/preview.ts
@@ -2,6 +2,44 @@ import type { Preview } from "@storybook/vue3";
 
 import "./tailwind.css";
 
+const viewports = {
+  sm: {
+    name: "sm",
+    styles: {
+      width: "640px",
+      height: "100%",
+    },
+  },
+  md: {
+    name: "md",
+    styles: {
+      width: "768px",
+      height: "100%",
+    },
+  },
+  lg: {
+    name: "lg",
+    styles: {
+      width: "1024px",
+      height: "100%",
+    },
+  },
+  xl: {
+    name: "xl",
+    styles: {
+      width: "1280px",
+      height: "100%",
+    },
+  },
+  "2xl": {
+    name: "2xl",
+    styles: {
+      width: "1536px",
+      height: "100%",
+    },
+  },
+};
+
 const preview: Preview = {
   parameters: {
     actions: { argTypesRegex: "^on[A-Z].*" },
@@ -10,6 +48,9 @@ const preview: Preview = {
         color: /(background|color)$/i,
         date: /Date$/,
       },
+    },
+    viewport: {
+      viewports,
     },
   },
 };
